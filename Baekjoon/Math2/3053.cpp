717bbd7320f1c777e1b69eac09a523decd94b13c
jams10 <jamin3710@gmail.com>
2020-03-07 16:28:49 +0000

    20200308 Solved

--- Baekjoon/Math2/3053.cpp
+++ Baekjoon/Math2/3053.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-#include <cmath>
+// #include <iostream>
+// #include <cmath>
 
-int main()
-{
-  double R=0;
-  std::cin>>R;
+// int main()
+// {
+//   double R=0;
+//   std::cin>>R;
 
-  double result = 3.1415926535897932 * R * R;
+//   double result = 3.1415926535897932 * R * R;
 
-  printf("%.6f %.6f",result, (2*R*2*R)/2.0);
-}
+//   printf("%.6f %.6f",result, (2*R*2*R)/2.0);
+// }
