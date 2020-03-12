86215c08880cbb65f29d11ec30d064a739ed7eb2
jams10 <jamin3710@gmail.com>
2020-03-12 05:17:25 +0000

    20200312 solved

--- Baekjoon/Brute_force/1018.cpp
+++ Baekjoon/Brute_force/1018.cpp
@@ -1,46 +1,62 @@
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
 
-int CheckBoard(const std::vector< std::vector<char> >& board, int row, int col)
-{
-  int paintingNumber=0;
-  for(; row<=row+7; ++row)
-  {
-    for(; col<=col+7; ++col)
-    {
-      // check slope. 2 cases first W or B
-    }
-  }
-}
+// int CheckBoard(const std::vector< std::vector<char> >& board, int row, int col)
+// {
+//   int paintWB=0, paintBW=0;
 
-int main()
-{
-  int n=0,m=0;
-  char tmp;
-  std::vector<int> cases;
-  std::vector< std::vector<char> > chessBoard;
-  std::cin>>n>>m;
+//   for(int r=row; r<=row+7; ++r)
+//   {
+//     for(int c=col; c<=col+7; ++c)
+//     {
+//       if((r+c)%2==0)
+//       {
+//         if(board[r][c]=='W')  paintWB++;
+//         else paintBW++;
+//       }
+//       else
+//       {
+//         if(board[r][c]=='B')  paintWB++;
+//         else paintBW++;
+//       }
+//     }
+//   }
+//   return paintWB<paintBW? paintWB : paintBW;
+// }
 
-  // 2D Vector
-  for(int row=0; row<n; ++row)
-  {
-    chessBoard.push_back(std::vector<char>());
-    for(int col=0; col<m; ++col)
-    {
-      std::cin>>tmp;
-      chessBoard.at(row).push_back(tmp);
-    }
-  }
+// int main()
+// {
+//   int n=0,m=0;
+//   char tmp;
+//   int min=0, result=2501;
+//   std::vector< std::vector<char> > chessBoard;
+//   std::cin>>n>>m;
 
-  // Check the board
-  for(int row=0; row<n; ++row)
-  {
-    for(int col=0; col<m; ++col)
-    {
-      if(row+7<n && col+7<m)
-      {
+//   // 2D Vector
+//   for(int row=0; row<n; ++row)
+//   {
+//     chessBoard.push_back(std::vector<char>());
+//     for(int col=0; col<m; ++col)
+//     {
+//       std::cin>>tmp;
+//       chessBoard.at(row).push_back(tmp);
+//     }
+//   }
 
-      }
-    }
-  }
-}
+//   // Check the board
+//   for(int row=0; row<n; ++row)
+//   {
+//     for(int col=0; col<m; ++col)
+//     {
+//       if(row+7<n && col+7<m)
+//       {
+//         min=CheckBoard(chessBoard, row, col);
+//         if(min<result)
+//         {
+//           result=min;
+//         }
+//       }
+//     }
+//   }
+//   std::cout<<result;
+// }
