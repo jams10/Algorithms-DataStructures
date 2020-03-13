c52b20e57c79c2f8aa9f98a54760a352159c6912
jams10 <jamin3710@gmail.com>
2020-03-13 10:12:49 +0000

    20200313 solved

---
+++ Baekjoon/Sort/2751_MergeSort.cpp
@@ -0,0 +1,79 @@
+// #include <iostream>
+// #include <list>
+// #include <stdio.h>
+
+// #define MAX_SIZE 1000001
+// int sortedArray[MAX_SIZE];
+
+// void Merge(int *pArray, int left, int mid, int right)
+// {
+//   int leftStart = left;
+//   int midStart = mid + 1;
+//   int index = left;
+
+//   while(leftStart <= mid && midStart <= right)
+//   {
+//     if(pArray[leftStart] <= pArray[midStart])
+//     {
+//       sortedArray[index] = pArray[leftStart];
+//       leftStart++;
+//     }
+//     else
+//     {
+//       sortedArray[index] = pArray[midStart];
+//       midStart++;
+//     }
+//     index++;
+//   }
+
+//   // copy remaining numbers
+//   if(leftStart > mid)
+//   {
+//     for(int i = midStart; i<=right; ++i)
+//     {
+//       sortedArray[index++] = pArray[i];
+//     }
+//   }
+//   else
+//   {
+//     for(int i = leftStart; i<=mid; ++i)
+//     {
+//       sortedArray[index++] = pArray[i];
+//     }
+//   }
+
+//   // copy numbers in sortedArray to pArray
+//   for(int i=left; i<=right; ++i)
+//   {
+//     pArray[i] = sortedArray[i];
+//   }
+// }
+// void MergeSort(int *pArray, int left, int right)
+// {
+//   if(left < right)
+//   {
+//     int mid = (left + right)/2;
+//     MergeSort(pArray, left, mid);
+//     MergeSort(pArray, mid + 1, right);
+//     Merge(pArray, left, mid, right);
+//   }
+// }
+
+// int main()
+// {
+//   int n=0;
+//   std::cin>>n;
+
+//   int *pArray = new int [n];
+//   for(int i=0; i<n; ++i)
+//   {
+//     scanf("%d",&pArray[i]);
+//   }
+
+//   MergeSort(pArray, 0, n-1);
+
+//   for(int i=0; i<n; ++i)
+//   {
+//     printf("%d\n",pArray[i]);
+//   }
+// }
---
+++ Baekjoon/Sort/2751_QuickSort.cpp
@@ -0,0 +1,74 @@
+// // Couldn't solve 2751 by using QuickSort...
+
+// #include <iostream>
+// #include <stdio.h>
+
+// int partition(int *pArray, int left, int right)
+// {
+//   int pivot = left;
+//   int i = left + 1;
+//   int j = right - 1;
+
+//   // Iterate until indices cross each other
+//   while(i <= j)
+//   {
+//     // left side numbers should be smaller than pivot number
+//     while(pArray[i]<=pArray[pivot] && i < right)
+//     {
+//       i++;
+//     }
+//     // left side numbers should be greater than pivot number
+//     while(pArray[j]>=pArray[pivot] && j > left)
+//     {
+//       j--;
+//     }
+//     // if left side number greater than pivot number
+//     // and right side number smaller than pivot number
+//     // and indices don't cross each other swap left, right side number
+//     if(i <= j)
+//     {
+//       // SWAP by using XOR bit operation (only for Integer)
+//       pArray[i]^=pArray[j];
+//       pArray[j]^=pArray[i];
+//       pArray[i]^=pArray[j];
+//     }
+//   }
+//   // if indices crossed each other, swap right side number and pivot number
+//   // cause, when indices crossed each other, 
+//   // the numbers of left side of 'j' index are smaller than pivot
+//   // SWAP by using function
+//   std::swap(pArray[pivot], pArray[j]);
+//   return j;
+// }
+
+
+// void quickSort(int *pArray, int left, int right)
+// {
+//   if(left < right)
+//   {
+//     int pivot = partition(pArray, left, right); // Conquer
+//     // because the sort of pivot index number is finished, do sort except it.
+//     quickSort(pArray, left, pivot); // Divide
+//     quickSort(pArray, pivot + 1, right); // Divide
+//   }
+// }
+
+// int main()
+// {
+//   int n=0;
+//   std::cin>>n;
+  
+//   int *pArray = new int[n];
+  
+//   for(int i=0; i<n; ++i)
+//   {
+//     scanf("%d", &pArray[i]);
+//   }
+
+//   quickSort(pArray, 0, n);
+
+//   for(int i=0; i<n; ++i)
+//   {
+//     printf("%d\n",pArray[i]);
+//   }
+// }
