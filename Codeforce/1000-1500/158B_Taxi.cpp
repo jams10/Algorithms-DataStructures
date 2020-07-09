a04e9288a8e5f3db2069cc0f034b38ec34c73d09
jams10 <jamin3710@gmail.com>
2020-07-09 05:55:49 +0000

    20200709 unsolved

--- Codeforce/1000-1500/158B_Taxi.cpp
+++ Codeforce/1000-1500/158B_Taxi.cpp
@@ -1,32 +1,67 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-
-bool comp(int a, int b)
-{
-  return a>b;
-}
 
 int main(int argc, char *argv[])
 {
   int n = 0, tmp = 0, count = 0;
+  int nFour = 0, nThree = 0, nTwo = 0, nOne = 0;
   std::cin>>n;
-
-  std::vector<int> v;
-  std::vector<int>::iterator iter;
 
   for(int i=0; i<n; i++)
   {
     std::cin>>tmp;
-    v.push_back(tmp);
+    if(tmp==4)
+    {
+      n_Four++;
+    }
+    else if(tmp==3)
+    {
+      n_Three++;
+    }
+    else if(tmp==2)
+    {
+      nTwo++;
+    }
+    else if(tmp==1)
+    {
+      nOne++;
+    }
   }
 
-  for(int i=0; i<v.size() i++)
+  //4
+  count+=nFour;
+
+  //3
+  if(nThree > 0)
   {
-    for(int j=0; j<v.size(); j++)
+    if(nThree >= nOne)
     {
-      if(v.at(j))
+      count += (nThree / nOne);
+      nThree = nThree % nOne ;
+      count += nThree;
     }
+    else
+    {
+      count += (nOne / nThree);
+      nOne = nOne % nThree;
+    }
+  }
+
+  //2
+  if(nTwo > 0)
+  {
+    if(nTwo >= nOne)
+    {
+      if(nOne % 2 == 0)
+      {
+        count += (nTwo)
+      }
+    }
+  }
+
+  //1
+  if(nOne > 0)
+  {
+    count+=nOne;
   }
 
   std::cout<<count;
