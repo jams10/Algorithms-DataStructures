c9e958156f261416605e1ce60e8ad86c5fb9bc71
jams10 <jamin3710@gmail.com>
2020-07-13 05:11:18 +0000

    20200713 solved

---
+++ Codeforce/1000-1500/270A_Fancy_Fence_1100.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+int main(int argc, char *argv[])
+{
+  int t = 0, angle = 0;
+  std::cin>>t;
+
+  for(int i=0; i<t; i++)
+  {
+    std::cin>>angle;
+    if(360 % ( 180-angle ) == 0) // result is integer
+    {
+      std::cout<<"YES";
+    }
+    else
+    {
+      std::cout<<"NO";
+    }
+  }
+
+  return 0;
+}
+
+/*
+The angle of the regular polygon 'a' is '180 * (n-2) / n'
+where n is the number of sides in the polygon.
+
+so, n should be an integer.
+
+a = 180 * (n-2) / n
+n = 360 / (180 - a)
+*/
