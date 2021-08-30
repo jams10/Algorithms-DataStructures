a2f96cad9f585d277a966652086d7fb6cf25edb5
jams10 <52204522+jams10@users.noreply.github.com>
2021-08-30 22:11:02 +0900

    Update Lotto_Ranking.cpp

--- Programmers/Lv1/Lotto_Ranking.cpp
+++ Programmers/Lv1/Lotto_Ranking.cpp
@@ -32,6 +32,12 @@ vector<int> solution(vector<int> lottos, vector<int> win_nums) {
     // 0이 없고 숫자가 다 다르면 match, zeros가 둘다 0이 됨. 여기서 -1을 하면 인덱스 에러가 일어나므로 이를 방지.
     int highest = match + zeros - 1 < 0 ? 0 : match + zeros - 1;
     int lowest = match - 1 < 0 ? 0 : match - 1;
+    
+    /*
+        다른 사람의 풀이를 보니 아래와 같이 배열을 안쓰고 함수 만든다음 그냥 계산해 버리는 방법도 있음.
+        if(num == 1 || num == 0) return 6;
+        else return 7 - num;
+    */
 
     answer.emplace_back( Ranking[highest] );
     answer.emplace_back( Ranking[lowest] );
