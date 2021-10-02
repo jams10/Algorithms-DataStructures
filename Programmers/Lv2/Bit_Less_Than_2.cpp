f2c836d54c29876c8865b52a5c9687cc3a1382ec
jams10 <52204522+jams10@users.noreply.github.com>
2021-10-02 18:31:57 +0900

    Update Bit_Less_Than_2.cpp

--- Programmers/Lv2/Bit_Less_Than_2.cpp
+++ Programmers/Lv2/Bit_Less_Than_2.cpp
@@ -23,6 +23,7 @@ vector<long long> solution( vector<long long> numbers )
             long long increment = 1;
             for( int j = 0; j < BITSIZE; ++j )
             {
+                //bitset 클래스의 경우 [] 오퍼레이터로 접근 시, 제일 하위 비트가 인덱스 0번
                 if( !n[j] )
                 {
                     answer.push_back( numbers[i] + increment / 2 );
