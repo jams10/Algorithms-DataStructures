ce72f59e5fb389de5c7e74944401ba2ba299aebb
jams10 <52204522+jams10@users.noreply.github.com>
2021-10-21 20:41:15 +0900

    Create Best_Album.cpp

---
+++ Programmers/Lv3/Best_Album.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+typedef struct
+{
+    int idx;
+    int plays;
+    string genre;
+}SONG;
+
+map<string, int> genrePlays;
+
+bool Compare(SONG song1, SONG song2)
+{
+  if( song1.genre != song2.genre )
+  {
+    return genrePlays[song1.genre] > genrePlays[song2.genre];
+  }
+  else
+  {
+    if(song1.plays != song2.plays) return song1.plays > song2.plays;
+    else return song1.idx < song2.idx;
+  }
+}
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+
+    int size = genres.size();
+
+    vector<SONG> songs;
+
+    for(int i = 0; i < size; ++i)
+    {
+        songs.push_back(SONG{i, plays[i], genres[i]});
+        genrePlays[genres[i]] += plays[i];
+    }
+
+    sort(songs.begin(), songs.end(), Compare);
+
+    map<string, int> genreCount;
+    for(int i = 0; i < size; ++i)
+    {
+        if(genreCount[songs[i].genre] < 2)
+        {
+          answer.push_back(songs[i].idx);
+          genreCount[songs[i].genre]++;
+        }
+    }
+
+    return answer;
+}
